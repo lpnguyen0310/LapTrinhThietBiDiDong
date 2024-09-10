3a9cac0b0cfbfc4da843aac9e89a5ed73713db08
DESKTOP-46K4QJ0\Dell <lp.nguyen0310@gmail.com>
2024-09-10 20:44:38 +0700

    FirstScreen + images

---
+++ Tuan03/GiaoDienApp/App.tsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import {StyleSheet, View, Image,Text,TouchableOpacity,Button} from 'react-native';
+
+const Flex = () => {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          // Try setting `flexDirection` to `"row"`.
+          flexDirection: 'column',
+        },
+      ]}>
+      <View style={styles.circleContainer} >
+         <Image style={styles.image}  source={require('./Ellipse 8.png')}/>
+      </View>
+      <Text style={styles.text}>GROW</Text>
+      <Text style={styles.title}>YOUR BUSINESS</Text>
+      
+      <Text style={styles.subtitle}>
+        We will help you to grow your business using online server
+      </Text>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#00CFFF'
+  },
+   circleContainer: {
+    flex:1,
+    alignItems: 'center',
+    marginTop:120,
+    marginBottom: 25,
+  },
+  image:{
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  text: {
+    marginTop:50,
+    fontSize: 25,
+    fontWeight: 'bold', 
+    color: 'black',
+    textAlign: 'center', 
+  },
+  title: {
+    flex:1,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+    
+  },
+  subtitle:{
+   flex: 1,
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center',
+    marginBottom: 40,
+    fontWeight: 'bold',
+    marginTop: -60,
+  // marginLeft: 0.7,
+  //  marginRight: 1,
+  },
+  buttonContainer: {
+    //flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    
+  
+    marginBottom: 50
+  },
+  button: {
+    backgroundColor: '#FFC300',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginHorizontal: 10,
+  },
+  buttonText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
+
+export default Flex;
