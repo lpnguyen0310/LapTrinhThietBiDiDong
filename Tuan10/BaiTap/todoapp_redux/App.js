44d0693a4531c98bb9c05c806b7942a22869750d
H51M14\Student <Student@gmail.com>
2024-11-01 08:39:12 +0700

    set redux to App

--- Tuan10/BaiTap/todoapp_redux/App.js
+++ Tuan10/BaiTap/todoapp_redux/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import store from './Store/index'; 
 import StartScreen from './components/StartScreen';
+import ToDoList from './components/ToDoList';
+import AddJobScreen from './components/AddJob';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="StartScreen" component={StartScreen} />
-        {/* Thêm các màn hình khác ở đây */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="ToDoList" component={ToDoList} />
+          <Stack.Screen name="AddJobScreen" component={AddJobScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
--- Tuan10/BaiTap/todoapp_redux/Store/index.js
+++ Tuan10/BaiTap/todoapp_redux/Store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
-import todoReducer from '../reducers/todoReducer';
+import todoReducer from '../slices/todoSlice';
 import rootSaga from '../sagas/ToDoSagas';
 
 const sagaMiddleware = createSagaMiddleware();
@@ -9,9 +9,10 @@ const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
-  middleware: [sagaMiddleware],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: false }).concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(rootSaga);
 
 export default store;
+
